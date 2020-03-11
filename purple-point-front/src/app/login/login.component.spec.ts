7804922b1723432c901e9cf52954a39a43e2e444
Claudia <claudia.sanchez.perez@est.fib.upc.edu>
2020-03-11 20:45:24 +0100

    created login component

---
+++ purple-point-front/src/app/login/login.component.spec.ts
@@ -0,0 +1,31 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginComponent } from './login.component';
+
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it(`should have a title 'example'`, async(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.debugElement.componentInstance;
+    expect(component.title).toEqual('example');
+  }));
+});
