ee1b2d6f204089fec8377fe95558c81017749a91
Claudia <claudia.sanchez.perez@est.fib.upc.edu>
2020-03-23 18:46:24 +0100

    started signup form

--- purple-point-front/src/app/app.module.ts
+++ purple-point-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignUpModule } from './sign-up/sign-up.module';
@@ -14,7 +15,8 @@ import { WelcomeModule } from './welcome/welcome.module';
     BrowserModule,
     AppRoutingModule,
     SignUpModule,
-    WelcomeModule
+    WelcomeModule,
+    FormsModule
     
   ],
   providers: [],
--- purple-point-front/src/app/sign-up/components/sign-up.component.ts
+++ purple-point-front/src/app/sign-up/components/sign-up.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  genders = ['Gender', 'Male', 'Female', 'Non binary', 'Ohter'];
   constructor() { }
 
   ngOnInit(): void {
