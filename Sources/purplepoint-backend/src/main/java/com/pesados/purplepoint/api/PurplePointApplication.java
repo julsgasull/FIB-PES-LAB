21e347f9fa53920dbb375a064190d6ae4e5dc679
IsmaelDLG <ismadlg.works@gmail.com>
2020-03-27 01:07:05 +0100

    test 9999999

--- Sources/purplepoint-backend/src/main/java/com/pesados/purplepoint/api/PurplePointApplication.java
+++ Sources/purplepoint-backend/src/main/java/com/pesados/purplepoint/api/PurplePointApplication.java
@@ -2,13 +2,10 @@ package com.pesados.purplepoint.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PurplePointApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PurplePointApplication.class, args);
 	}
