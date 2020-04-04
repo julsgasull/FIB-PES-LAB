3019e3cb7c7a4ef6c8ef4448d96656fd4e17f8c9
Júlia <juliagasull@icloud.com>
2020-04-04 12:14:22 +0200

    main menu image fixes

--- purple-point-front/src/app/main-menu/main-menu.component.ts
+++ purple-point-front/src/app/main-menu/main-menu.component.ts
@@ -1,4 +1,9 @@
+declare var require: any
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user/user.service';
+import { UserData } from 'src/app/models/userData.interface';
+
 
 @Component({
   selector: 'app-main-menu',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMenuComponent implements OnInit {
 
-  constructor() { }
+  // images
+  profileImage/* = require(agafar foto de perfil de la bd)*/;
+  expandImage   = require("../../../images/expand.svg");
+  mapImage      = require("../../../images/map.svg");
+  wikiImage     = require("../../../images/wiki.svg");
+
+  public userInfo: UserData;
+
+  constructor(
+    private route: Router,
+    private userService: UserService
+  ) {}
+
 
   ngOnInit(): void {
+    const userEmail = localStorage.getItem('userEmail');
+    this.userService.getUserByEmail(userEmail).subscribe((response: UserData) => {
+      this.userInfo = response;
+    });
   }
 
+  redirectToMap() {
+    //to-do
+  }
+  redirectToWiki() {
+    //to-do
+  }
 }
