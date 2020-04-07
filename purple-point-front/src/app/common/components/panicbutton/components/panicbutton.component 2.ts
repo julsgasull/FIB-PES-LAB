e2a8c6f3dfb53c4267d5ae2fe30b41f910d61ee6
Júlia <juliagasull@icloud.com>
2020-04-07 16:04:11 +0200

    panic button

---
+++ purple-point-front/src/app/common/components/panicbutton/components/panicbutton.component 2.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'panicbutton',
+  templateUrl: './panicbutton.component.html',
+  styleUrls: ['./panicbutton.component.scss']
+})
+export class PanicbuttonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
