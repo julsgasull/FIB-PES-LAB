e0e392ad0592058e3b230b2f510b71a6d7d1b101
Claudia <claudia.sanchez.perez@est.fib.upc.edu>
2020-04-12 19:52:39 +0200

    added the interceptor to avoid the token header

--- purple-point-front/src/app/services/auth/auth.interceptor.ts
+++ purple-point-front/src/app/services/auth/auth.interceptor.ts
@@ -3,6 +3,9 @@ import { Injectable, Injector } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
+
+export const InterceptorSkipHeader = 'X-Skip-Interceptor';
+
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
@@ -10,11 +13,16 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const auth = this.injector.get(AuthService);
-    request = request.clone({
-      setHeaders: {
-        Authorization: `${auth.getToken()}`
-      }
-    });
-    return next.handle(request);
+    if (request.headers.has(InterceptorSkipHeader)) {
+      const headers = request.headers.delete(InterceptorSkipHeader);
+      return next.handle(request.clone({ headers }));
+    } else {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `${auth.getToken()}`
+        }
+      });
+      return next.handle(request);
+    }
   }
 }
