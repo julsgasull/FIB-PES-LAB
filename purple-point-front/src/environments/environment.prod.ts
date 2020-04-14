1f2ab66a7011511a4720b1a489e7f06470939d76
Claudia <claudia.sanchez.perez@est.fib.upc.edu>
2020-04-14 08:55:29 +0200

    changed API IP

--- purple-point-front/src/environments/environment.prod.ts
+++ purple-point-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'http://10.4.41.153/api/v1'
+  API_URL: 'http://10.4.41.147/api/v1'
 };
--- purple-point-front/src/environments/environment.ts
+++ purple-point-front/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: false,
-  API_URL: 'http://10.4.41.153/api/v1',
-  BASE_URL_AUTHENTICATION: /* falta la url de authentication */ ''
+  API_URL: 'http://10.4.41.147/api/v1'
 };
