c524413a1fb9cda93e5bb77506ddf4eb20e92287
Júlia <juliagasull@icloud.com>
2020-04-22 13:28:44 +0200

    prettier

--- purple-point-front/src/app/services/user/user.remote.ts
+++ purple-point-front/src/app/services/user/user.remote.ts
@@ -3,6 +3,8 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { UserData } from 'src/app/models/userData.interface';
+import { ProfilePicData } from 'src/app/models/profilepicdata.interface';
+
 
 @Injectable()
 export class UserRemote {
@@ -28,7 +30,7 @@ export class UserRemote {
     login(user: UserData): Observable<UserData> {
         return this.httpClient.post<UserData>(`${environment.API_URL}/users/login`, 
         {   
-            'email': user.email,
+            'email':    user.email,
             'password': user.password
         },
         {
@@ -40,22 +42,22 @@ export class UserRemote {
     }
 
     getUserByEmail(email: string): Observable<UserData> {
-        return this.httpClient.get<UserData>(`${environment.API_URL}/users/email/`+email
-        );
+        return this.httpClient.get<UserData>(`${environment.API_URL}/users/email/`+email);
     }
 
     editProfile(user: UserData): Observable<any> {
         return this.httpClient.put<UserData>(`${environment.API_URL}/users/email/`+user.email,
         {   
-            'id': user.id,
-            'name': user.name,
-            'email': user.email,
-            'username': user.username,
-            'password': user.password,
-            'gender': user.gender,
-            'token': user.token,
-            'markedSpots': user.markedSpots,
-            'helpedUsers': user.helpedUsers
+            'id':           user.id,
+            'name':         user.name,
+            'email':        user.email,
+            'username':     user.username,
+            'password':     user.password,
+            'gender':       user.gender,
+            'token':        user.token,
+            'markedSpots':  user.markedSpots,
+            'helpedUsers':  user.helpedUsers,
+            'profilePic':   user.profilePic
         });
     }
 }
