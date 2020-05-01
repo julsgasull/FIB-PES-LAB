cbf57a6a031aef604ef3a52cd59c603bf9facabf
Júlia <juliagasull@icloud.com>
2020-05-01 11:38:54 +0200

    marked spot visible

--- purple-point-front/src/app/map/components/map.component.ts
+++ purple-point-front/src/app/map/components/map.component.ts
@@ -1,5 +1,6 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as L from 'leaflet';
+import { MarkerService } from 'src/app/services/marker/marker.service';
 
 @Component({
   selector: 'app-map',
@@ -7,17 +8,18 @@ import * as L from 'leaflet';
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements AfterViewInit {
-  private map: any;
+  private map: L.Map;
 
-  constructor() { }
+  constructor(private markerService: MarkerService) { }
 
   ngAfterViewInit(): void {
     this.initMap();
+    this.markerService.getAllMarks(this.map);
   }
 
   initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
+      center: [ 51.5, -0.09 ],
       zoom: 3
     });
     var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
--- purple-point-front/src/app/services/marker/marker.service.ts
+++ purple-point-front/src/app/services/marker/marker.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
+import { HttpClient } from '@angular/common/http';
+
+var greenIcon = L.icon({
+  iconUrl: '../../../assets/images/pin.svg',
+  shadowUrl: '../../../assets/images/pin-shadow.svg',
+
+  iconSize:     [38, 95], // size of the icon
+  shadowSize:   [50, 64], // size of the shadow
+  iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4,  62], // the same for the shadow
+  popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  getAllMarks(map: L.Map) {
+    L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map)
+      .bindPopup('this is a test mark')
+      .openPopup()
+    ;
+  }
+  
 }
