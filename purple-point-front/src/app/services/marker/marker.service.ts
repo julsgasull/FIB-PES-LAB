b4c9b145d25ec68da9662337f61f66b94ca2a354
Júlia <juliagasull@icloud.com>
2020-05-01 09:59:40 +0200

    map view

--- purple-point-front/src/app/services/marker/marker.service.ts
+++ purple-point-front/src/app/services/marker/marker.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
 
-  capitals: string = '/assets/data/usa-capitals.geojson';
-
-  constructor(private http: HttpClient) {
-  }
+  constructor() { }
 }
