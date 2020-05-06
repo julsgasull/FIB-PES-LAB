b55926c4e61834dcbc7c01bbaf91aca37c7958ab
arle21 <franco.junior.araujo@est.fib.upc.edu>
2020-05-06 17:37:43 +0200

    Firebase setup all test runnig except the alarm one

--- Sources/purplepoint-backend/src/main/java/com/pesados/purplepoint/api/model/firebase/PushNotificationService.java
+++ Sources/purplepoint-backend/src/main/java/com/pesados/purplepoint/api/model/firebase/PushNotificationService.java
@@ -1,5 +1,6 @@
 package com.pesados.purplepoint.api.model.firebase;
 
+import com.google.firebase.messaging.FirebaseMessagingException;
 import com.pesados.purplepoint.api.firebase.FCMService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,16 +9,19 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class PushNotificationService {
 
+    /*
     @Value("#{${app.notifications.defaults}}")
     private Map<String, String> defaults;
-
+    */
     private Logger logger = LoggerFactory.getLogger(PushNotificationService.class);
     private FCMService fcmService;
 
@@ -25,6 +29,7 @@ public class PushNotificationService {
         this.fcmService = fcmService;
     }
 
+    /*
     @Scheduled(initialDelay = 60000, fixedDelay = 60000)
     public void sendSamplePushNotification() {
         try {
@@ -41,7 +46,7 @@ public class PushNotificationService {
             logger.error(e.getMessage());
         }
     }
-
+    */
     public void sendPushNotificationWithoutData(PushNotificationRequest request) {
         try {
             fcmService.sendMessageWithoutData(request);
@@ -50,6 +55,15 @@ public class PushNotificationService {
         }
     }
 
+    public void sendMulticastPushNotification(PushNotificationRequest request, List<String> tokens){
+
+        try {
+            fcmService.sendMulticastMessageWithoutData(request, tokens);
+        } catch (FirebaseMessagingException e) {
+            logger.error(e.getMessage());
+        }
+
+    }
 
     public void sendPushNotificationToToken(PushNotificationRequest request) {
         try {
@@ -59,7 +73,7 @@ public class PushNotificationService {
         }
     }
 
-
+    /*
     private Map<String, String> getSamplePayloadData() {
         Map<String, String> pushData = new HashMap<>();
         pushData.put("messageId", defaults.get("payloadMessageId"));
@@ -67,13 +81,14 @@ public class PushNotificationService {
         return pushData;
     }
 
-
     private PushNotificationRequest getSamplePushNotificationRequest() {
         PushNotificationRequest request = new PushNotificationRequest(defaults.get("title"),
                 defaults.get("message"),
                 defaults.get("topic"));
         return request;
     }
+
+     */
 
 
 }
