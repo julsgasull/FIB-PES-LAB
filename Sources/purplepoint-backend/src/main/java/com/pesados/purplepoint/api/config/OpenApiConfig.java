5387a48dfa71f8e38e596b6b24d377faccd00b7b
arle21 <franco.junior.araujo@est.fib.upc.edu>
2020-05-08 16:48:36 +0200

    Server url changed

--- Sources/purplepoint-backend/src/main/java/com/pesados/purplepoint/api/config/OpenApiConfig.java
+++ Sources/purplepoint-backend/src/main/java/com/pesados/purplepoint/api/config/OpenApiConfig.java
@@ -1,5 +1,8 @@
-
 package com.pesados.purplepoint.api.config;
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -9,32 +12,32 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import java.util.ArrayList;
 
 @OpenAPIDefinition(
-        security = {@SecurityRequirement(name = "bearer")}
+		security = {@SecurityRequirement(name = "bearer")}
 )
 @SecurityScheme(name = "bearer", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
 @Configuration
 public class OpenApiConfig {
-    @Bean
-    public OpenAPI customOpenAPI() {
-    	Server myServer = new Server();
-    	myServer.setUrl("http://10.4.41.147/");
-    	ArrayList<Server> serverList = new ArrayList<Server> ();
-    	serverList.add(myServer);
-        myServer.setUrl("http://localhost:5001/");
-        serverList.add(myServer);
+	@Bean
+	public OpenAPI customOpenAPI() {
+		/*------------------ Servers Config ------------------*/
+		Server myServer = new Server();
+		myServer.setUrl("http://10.4.41.147/");
+		myServer.setDescription("Development server");
+		ArrayList<Server> serverList = new ArrayList<Server> ();
+		serverList.add(myServer);
 
-        return new OpenAPI()
-                .components(new Components())
-                .servers(serverList)
-                .info(new Info().title("PurplePoint Application API").description(
-                        "This is PurplePoint's Spring Boot API service using springdoc-openapi and OpenAPI 3.")
-						.version("v1"));
-    }
+		/*------------------ Security Config ------------------*/
+
+		/*------------------ END ------------------*/
+
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("PurplePoint Application API").description(
+						"This is PurplePoint's Spring Boot API service using springdoc-openapi and OpenAPI 3."))
+				.openapi("3.0.0")
+				.servers(serverList);
+	}
 
 }
