721594d50b570830510b2cda5d2ab52449de7e31
Amanda <amandaverdugoacosta@gmail.com>
2020-05-13 12:45:13 +0200

    New endpoint for updating firebaseToken
    Tests passed.

--- Sources/purplepoint-backend/src/test/java/com/pesados/purplepoint/api/tests/controller/DeviceControllerTest.java
+++ Sources/purplepoint-backend/src/test/java/com/pesados/purplepoint/api/tests/controller/DeviceControllerTest.java
@@ -1,6 +1,9 @@
 package com.pesados.purplepoint.api.tests.controller;
 
+import com.pesados.purplepoint.api.model.device.Device;
+import com.pesados.purplepoint.api.model.device.DeviceService;
 import org.json.JSONObject;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,6 +24,9 @@ public class DeviceControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    DeviceService deviceService;
 
     @Test
     public void shouldReturnUnauthorized() throws Exception {
@@ -72,11 +78,22 @@ public class DeviceControllerTest {
     }
 
     @Test
-    public void shouldsendNotification() throws Exception{
+    public void shouldSendNotification() throws Exception{
+        // Login with mockup user in the database.
+        JSONObject location = new JSONObject();
+        location.put("latitude", 5.0);
+        location.put("longitude", 7.0);
+        location.put("accuracy", 9.0);
+        location.put("timestamp", 20.0);
 
         JSONObject user = new JSONObject();
         user.put("email", "isma@gmail.com");
         user.put("password", "1234");
+
+        JSONObject device = new JSONObject();
+        device.put("firebaseToken", "f2EJYEQeYyYq-v2ubvL7x5:APA91bFam-no_lk9-kryCZol_dXDEtRjyd_iyAORuLDuLgLmyblUhYE9sYV1Prj4ohxnt6-EM_tDBVOkhnV08e2szqCGjNBRap5vnRwzBVf0iCMzlCphZiAWCkRWiDx0pB71dZEj2Ej5");
+        device.put("location", location);
+        device.put("user", user);
 
         MvcResult response = this.mockMvc.perform(MockMvcRequestBuilders
                 .post("/api/v1/users/login")
@@ -92,8 +109,38 @@ public class DeviceControllerTest {
                 .param("username", "Isma"))
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(status().is(200));
+    }
 
 
+    @Test
+    public void shouldUpdateToken() throws Exception {
+        // Login with mockup user in the database.
+        JSONObject user = new JSONObject();
+        user.put("email", "isma@gmail.com");
+        user.put("password", "1234");
+
+        MvcResult response = this.mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/users/login")
+                .contentType("application/json")
+                .content(user.toString()))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        JSONObject respUser = new JSONObject(response.getResponse().getContentAsString());
+        String token =((String) respUser.get("token"));
+
+        Device newDevice = deviceService.getDeviceByFirebaseToken("f2EJYEQeYyYq-v2ubvL7x5:APA91bFam-no_lk9-kryCZol_dXDEtRjyd_iyAORuLDuLgLmyblUhYE9sYV1Prj4ohxnt6-EM_tDBVOkhnV08e2szqCGjNBRap5vnRwzBVf0iCMzlCphZiAWCkRWiDx0pB71dZEj2Ej5").get();
+        long idDevice = newDevice.getDeviceId();
+
+        this.mockMvc.perform(put("/api/v1/device/updatetoken/f2EJYEQeYyYq-v2ubvL7x5:APA91bFam-no_lk9-kryCZol_dXDEtRjyd_iyAORuLDuLgLmyblUhYE9sYV1Prj4ohxnt6-EM_tDBVOkhnV08e2szqCGjNBRap5vnRwzBVf0iCMzlCphZiAWCkRWiDx0pB71dZEj2Ej5").header("Authorization",token)
+                .content("1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().is(200));
+
+        newDevice = deviceService.getDeviceById(idDevice).get();
+        Assert.assertEquals(newDevice.getFirebaseToken(), "f2EJYEQeYyYq-v2ubvL7x5:APA91bFam-no_lk9-kryCZol_dXDEtRjyd_iyAORuLDuLgLmyblUhYE9sYV1Prj4ohxnt6-EM_tDBVOkhnV08e2szqCGjNBRap5vnRwzBVf0iCMzlCphZiAWCkRWiDx0pB71dZEj2Ej5");
     }
 
 }
