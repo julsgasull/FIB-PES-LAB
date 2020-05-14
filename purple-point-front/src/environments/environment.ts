62ceb5fc325e58eb6265246c41e0e9e19b5e6833
Aleix Ventura Nasarre <aleix.ventura.nasarre@est.fib.upc.edu>
2020-05-14 16:13:15 +0200

    Changed to heroku
    API_URL ahora apunta a heroku

--- purple-point-front/src/environments/environment.ts
+++ purple-point-front/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: 'http://10.4.41.147/api/v1',
+  //API_URL: 'http://10.4.41.147/api/v1',
+  API_URL: 'https://purplepoint.herokuapp.com/',
   firebase: {
     apiKey: "AIzaSyBdO7h3Nkq1tTQIm_UPlUgEhBrlUFevoRE",
     authDomain: "purplepoint-f2abf.firebaseapp.com",
