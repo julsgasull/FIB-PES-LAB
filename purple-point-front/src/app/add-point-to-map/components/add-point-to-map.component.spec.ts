32901cba09bcf5e401760bd321cb5e7b72e40d82
Claudia <claudia.sanchez.perez@est.fib.upc.edu>
2020-05-26 16:59:31 +0200

    unit tests

--- purple-point-front/src/app/add-point-to-map/components/add-point-to-map.component.spec.ts
+++ purple-point-front/src/app/add-point-to-map/components/add-point-to-map.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AddPointToMapComponent } from './add-point-to-map.component';
+import { LanguageButtonModule } from 'src/app/common/components/language-button/language-button.module';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TranslateModule, TranslateLoader, TranslateFakeLoader } from '@ngx-translate/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CommonModule } from '@angular/common';
+import { UserRemote } from 'src/app/services/user/user.remote';
 
 describe('AddPointToMapComponent', () => {
   let component: AddPointToMapComponent;
@@ -8,7 +14,22 @@ describe('AddPointToMapComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddPointToMapComponent ]
+      declarations: [ AddPointToMapComponent ],
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useClass: TranslateFakeLoader
+          }
+        }),
+        HttpClientTestingModule,
+        CommonModule,
+        LanguageButtonModule
+      ],
+      providers: [
+        UserRemote
+      ]
     })
     .compileComponents();
   }));
--- purple-point-front/src/app/common/components/simple-snackbar/components/simple-snackbar.component.spec.ts
+++ purple-point-front/src/app/common/components/simple-snackbar/components/simple-snackbar.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { MAT_SNACK_BAR_DATA, MatSnackBarRef } from '@angular/material/snack-bar';
 import { SimpleSnackbarComponent } from './simple-snackbar.component';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { NotificationsRemote } from 'src/app/services/notifications/notifications.remote';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { UserRemote } from 'src/app/services/user/user.remote';
 
 describe('SimpleSnackbarComponent', () => {
   let component: SimpleSnackbarComponent;
@@ -8,7 +13,18 @@ describe('SimpleSnackbarComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SimpleSnackbarComponent ]
+      declarations: [ SimpleSnackbarComponent ],
+      imports:[
+        OverlayModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        BrowserDynamicTestingModule,
+        { provide: MAT_SNACK_BAR_DATA, useValue: {} },
+        { provide: MatSnackBarRef, useValue: {} },
+        NotificationsRemote,
+        UserRemote
+      ]
     })
     .compileComponents();
   }));
