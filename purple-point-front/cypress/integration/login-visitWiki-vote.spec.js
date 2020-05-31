0301efd4bf8c39aa6fd87e89cbec7a0ffd40d836
Claudia <claudia.sanchez.perez@est.fib.upc.edu>
2020-05-31 17:16:37 +0200

    anadir punto al mapa funciona, falta eliminar y mover y los de la wiki

--- purple-point-front/cypress/integration/login-visitWiki-vote.spec.js
+++ purple-point-front/cypress/integration/login-visitWiki-vote.spec.js
@@ -17,21 +17,35 @@ describe('Testing Purple point front login error, visitMap and add a point', () 
         cy.get('#password')
         .type('1234');
         cy.wait(1000);
-        cy.get('.principalButton')
-        .click();
+
+        cy.server()
+        cy.route({
+            method: 'POST',      // Route all GET requests
+            url: 'https://purplepoint.herokuapp.com/api/v1/users/login',
+            response: ['fixture:users.json']        // and force the response to be: []
+        }).as('login');
+        cy.get('.principalButton').click();
+        cy.wait('@login').its('status').should('eq', 200);
         cy.wait(1000);
+        cy.visit('http://localhost:4200/mainmenu');
         cy.url().should('include', 'mainmenu');
     });
 
     it('MainMenu should redirect to wiki and vote a concept', () => {
         // falta redirigir hacia wiki y poder votar un concepto
+        cy.get('#redirectToWiki').click();
+        cy.url().should('include', 'wikifaq');
+         
     });
 
     it('MainMenu should redirect to wiki and unvote a concept', () => {
         // falta redirigir hacia wiki y poder desvotar un concepto
     });
 
-    it('MainMenu should redirect to the concept selected', () => {
-        // falta redirigir hacia el concepto seleccionado
+    it('MainMenu should redirect to phones and definitions', () => {
+        cy.get('#wikiphonesRedirect').click();
+        cy.url().should('include', 'wikiphones');
+        cy.get('#wikidefinitionsRedirect').click();
+        cy.url().should('include', 'wikidefinitions');
     });
 });
--- purple-point-front/cypress/integration/loginerror-visitMap-addPoint.spec.js
+++ purple-point-front/cypress/integration/loginerror-visitMap-addPoint.spec.js
@@ -33,14 +33,34 @@ describe('Testing Purple point front login error, visitMap and add a point', () 
         .clear()
         .type('1234');
         cy.wait(1000);
+        cy.server()
+        cy.route({
+            method: 'POST',      // Route all GET requests
+            url: 'https://purplepoint.herokuapp.com/api/v1/users/login',
+            response: ['fixture:users.json']        // and force the response to be: []
+            }).as('login')
         cy.get('.principalButton')
         .click();
-        cy.wait(1000);
+        cy.wait('@login').its('status').should('eq', 200);
+        cy.visit('http://localhost:4200/mainmenu');
         cy.url().should('include', 'mainmenu');
     });
 
     it('MainMenu should redirect to map and add a new spot', () => {
-        // falta redirigir hacia mapa y poder añadir nuevo punto al mapa
+        cy.get('#redirectToMap').click();
+        cy.url().should('include', 'map');
+        cy.get('#addPointRedirect').click();
+        cy.url().should('include', 'addpointtotmap');
+        cy.wait(2000);
+        cy.server()
+        cy.route({
+            method: 'POST',      // Route all GET requests
+            url: 'https://purplepoint.herokuapp.com/api/v1/map',
+            response: ['']        // and force the response to be: []
+        }).as('addPoint');
+        cy.get('#map').click(41.48073166690805, 1.893368408146361);
+        cy.wait('@addPoint').its('status').should('eq', 200);
+        cy.url().should('include', 'map');
     });
 
     it('MainMenu should redirect to map and remove a new spot', () => {
